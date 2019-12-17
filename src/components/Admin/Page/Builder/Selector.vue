504095a18f8f6685ed8b90e2b79836ace8840f2b
Denis Arh <denis.arh@gmail.com>
2019-12-17 15:23:48 +0100

    Refactor block-type selector

--- src/components/Admin/Page/Builder/Selector.vue
+++ src/components/Admin/Page/Builder/Selector.vue
@@ -27,34 +27,47 @@ export default {
   data () {
     return {
       types: [
-        { label: this.$t('block.content.label'),
-          recordPageOnly: false,
-          block: new Block({ kind: 'Content' }) },
-        { label: this.$t('block.chart.label'),
-          recordPageOnly: false,
-          block: new Block({ kind: 'Chart' }) },
-        { label: this.$t('block.socialFeed.label'),
-          recordPageOnly: false,
-          block: new Block({ kind: 'SocialFeed' }) },
-        { label: this.$t('block.record.label'),
+        {
+          label: this.$t('block.content.label'),
+          block: new Block({ kind: 'Content' }),
+        },
+        {
+          label: this.$t('block.chart.label'),
+          block: new Block({ kind: 'Chart' }),
+        },
+        {
+          label: this.$t('block.socialFeed.label'),
+          block: new Block({ kind: 'SocialFeed' }),
+        },
+        {
+          label: this.$t('block.record.label'),
+          block: new Block({ kind: 'Record' }),
           recordPageOnly: true,
-          block: new Block({ kind: 'Record' }) },
-        { label: this.$t('block.recordList.label'),
-          recordPageOnly: false,
-          block: new Block({ kind: 'RecordList' }) },
-        { label: this.$t('block.automation.label'),
-          recordPageOnly: false,
-          block: new Block({ kind: 'Automation' }) },
-        { label: this.$t('block.calendar.label'),
-          recordPageOnly: false,
-          block: new Block({ kind: 'Calendar' }) },
-        { label: this.$t('block.file.label'),
-          recordPageOnly: false,
-          block: new Block({ kind: 'File' }) },
-        { label: this.$t('block.recordOrganizer.label'),
-          recordPageOnly: false,
-          block: new Block({ kind: 'RecordOrganizer' }) },
-      ],
+        },
+        {
+          label: this.$t('block.recordList.label'),
+          block: new Block({ kind: 'RecordList' }),
+        },
+        {
+          label: this.$t('block.automation.label'),
+          block: new Block({ kind: 'Automation' }),
+        },
+        {
+          label: this.$t('block.calendar.label'),
+          block: new Block({ kind: 'Calendar' }),
+        },
+        {
+          label: this.$t('block.file.label'),
+          block: new Block({ kind: 'File' }),
+        },
+        {
+          label: this.$t('block.recordOrganizer.label'),
+          block: new Block({ kind: 'RecordOrganizer' }),
+        },
+      ].map(type => ({
+        ...type,
+        recordPageOnly: false,
+      })),
     }
   },
 }
